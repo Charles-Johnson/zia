4ec01845b424f859c5655ba0f4e34eb6ce877866
Charles Johnson <charlesthomasjohnson0@gmail.com>
2018-08-29 18:04:04 +0100

    Wrote the ffi for memory_database and oracle, deleted any functionality for reading a database from file, as this will be replaced using the safe_client library

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,19 @@ use tree::extract_tree_from_token;
 
 use token::Token;
 
+#[no_mangle]
+pub extern fn memory_database_ffi() -> SqliteConnection {
+    memory_database().unwrap()
+}
+
+#[no_mangle]
+pub extern fn oracle_ffi(buffer: &str, conn: &SqliteConnection) -> String {
+    match oracle(buffer, conn) {
+        Ok(s) => s,
+        Err(e) => e.to_string()
+    }
+}
+
 pub fn oracle(buffer: &str, conn: &SqliteConnection)-> ZiaResult<String> {
     let tree = try!(extract_tree_from_token(&Token::Expression(buffer.to_string()), conn));
     let mut string = String::new();
@@ -25,7 +38,7 @@ mod reductions {
     use {oracle, memory_database};
     #[test]
     fn monad() {
-        let (conn, _file_conn) = memory_database().unwrap();
+        let conn = memory_database().unwrap();
         assert_eq!(oracle("(-> b)a", &conn).unwrap(),"");
         assert_eq!(oracle("a ->", &conn).unwrap(),"b");
         assert_eq!(oracle("(-> false)(not true)", &conn).unwrap(), "");
@@ -33,26 +46,26 @@ mod reductions {
     }
     #[test]
     fn nested_monads() {
-        let (conn, _file_conn) = memory_database().unwrap();
+        let conn = memory_database().unwrap();
         assert_eq!(oracle("(-> false)(not true)", &conn).unwrap(), "");
         assert_eq!(oracle("(-> true)(not false)", &conn).unwrap(), "");
         assert_eq!(oracle("(not(not true))->", &conn).unwrap(), "true");
     }
     #[test]
     fn chain() {
-        let (conn, _file_conn) = memory_database().unwrap();
+        let conn = memory_database().unwrap();
         assert_eq!(oracle("(-> b) a", &conn).unwrap(),"");
         assert_eq!(oracle("(-> c) b", &conn).unwrap(), "");
         assert_eq!(oracle("a ->", &conn).unwrap(), "c")
     }
     #[test]
     fn diad() {
-        let (conn, _file_conn) = memory_database().unwrap();
+        let conn = memory_database().unwrap();
         assert_eq!(oracle("(0 + 1)->", &conn).unwrap(), "1");
     }
     #[test]
     fn set_precedence() {
-        let (conn, _file_conn) = memory_database().unwrap();
+        let conn = memory_database().unwrap();
         assert_eq!(oracle("(>- b) a", &conn).unwrap(), "");
         assert_eq!(oracle("((>- b) a) ->", &conn).unwrap(), "true");
     }
@@ -63,13 +76,13 @@ mod definitions {
     use memory_database;
     #[test]
     fn monad() {
-        let (conn, _file_conn) = memory_database().unwrap();
+        let conn = memory_database().unwrap();
         assert_eq!(oracle("(:= (repeated +))*", &conn).unwrap(), "");
         assert_eq!(oracle("* :=", &conn).unwrap(), "repeated +");
     }
     #[test]
     fn nested_monads() {
-        let (conn, _file_conn) = memory_database().unwrap();
+        let conn = memory_database().unwrap();
         assert_eq!(oracle("(:= (++ (++ 0)))2", &conn).unwrap(), "");
         assert_eq!(oracle("2 :=", &conn).unwrap(), "++ (++ 0)");
     }
