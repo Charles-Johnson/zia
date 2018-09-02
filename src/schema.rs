aead0f0c85c088fb42af3e5431b8e827a295aeae
Charles Johnson <charlesthomasjohnson0@gmail.com>
2018-09-02 15:27:39 +0100

    Included untracked files

---
+++ src/schema.rs
@@ -0,0 +1,35 @@
+table! {
+    definitions (id) {
+        id -> Integer,
+        applicant -> Integer,
+        argument -> Integer,
+    }
+}
+
+table! {
+    integers (id) {
+        id -> Integer,
+        result -> Integer,
+    }
+}
+
+table! {
+    reductions (id) {
+        id -> Integer,
+        normal_form -> Integer,
+    }
+}
+
+table! {
+    text (id) {
+        id -> Integer,
+        result -> Text,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    definitions,
+    integers,
+    reductions,
+    text,
+);
