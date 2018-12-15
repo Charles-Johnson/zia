2eed4ca49dc9a7b74252b7c9c2a33d577a7664a2
Charles Johnson <charlesthomasjohnson0@gmail.com>
2018-12-15 14:55:35 +0900

    Defined a module within combined_traits called concept_maker which contains all traits that only depend on ConceptAdder or StringAdder.

--- src/combined_traits.rs
+++ src/combined_traits.rs
@@ -15,17 +15,17 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 use ast::traits::{
-    Container as SyntaxContainer, MightExpand, SyntaxFactory,
+    Container as SyntaxContainer,
 };
+use self::concept_maker::{AbstractMaker, AbstractFactory, StringMaker, StringFactory};
 use self::concept_reader::{ConceptReader, GetReduction, GetDefinition, GetDefinitionOf,
-	FindWhatReducesToIt, FindDefinition, Container, MaybeDisconnected, GetConceptOfLabel, Label, GetNormalForm, Combine, Pair, DisplayJoint, MaybeConcept, };
+	FindWhatReducesToIt, FindDefinition, Container, MaybeDisconnected, GetConceptOfLabel, Label, GetNormalForm, Combine, Pair, DisplayJoint, MaybeConcept, MaybeString, GetLabel, Reduce, MightExpand, SyntaxFactory, Expander};
 use concepts::traits::{
-    AbstractFactory, MaybeString, RemoveDefinition, RemoveReduction, SetDefinition, SetReduction,
-    StringFactory,
+    RemoveDefinition, RemoveReduction, SetDefinition, SetReduction,
 };
 use constants::{DEFINE, LABEL, REDUCTION};
 use context::traits::{
-    ConceptAdder, ConceptRemover, ConceptWriter, StringAdder, StringConcept,
+    ConceptRemover, ConceptWriter, StringConcept,
 };
 use std::fmt;
 use token::parse_line;
@@ -816,26 +816,6 @@ where
 {
 }
 
-pub trait StringMaker<T>
-where
-    T: StringFactory + MaybeString,
-    Self: ConceptAdder<T> + StringAdder,
-{
-    fn new_string(&mut self, string: &str) -> usize {
-        let string_concept = T::new_string(string);
-        let index = self.add_concept(string_concept);
-		self.add_string(index, string);
-		index
-    }
-}
-
-impl<S, T> StringMaker<T> for S
-where
-    T: StringFactory + MaybeString,
-    S: ConceptAdder<T> + StringAdder,
-{
-}
-
 pub trait FindOrInsertDefinition<T>
 where
     T: AbstractFactory + GetDefinition + GetReduction + SetDefinition + GetDefinitionOf,
@@ -905,233 +885,253 @@ where
 {
 }
 
-pub trait AbstractMaker<T>
-where
-    T: AbstractFactory,
-    Self: ConceptAdder<T>,
-{
-    fn new_abstract(&mut self) -> usize {
-        let concept = T::new_abstract();
-        self.add_concept(concept)
-    }
-}
+mod concept_maker {
+	use context::traits::{ConceptAdder, StringAdder};
+	pub use concepts::traits::{AbstractFactory, StringFactory};
+	pub trait StringMaker<T>
+	where
+		T: StringFactory,
+		Self: ConceptAdder<T> + StringAdder,
+	{
+		fn new_string(&mut self, string: &str) -> usize {
+		    let string_concept = T::new_string(string);
+		    let index = self.add_concept(string_concept);
+			self.add_string(index, string);
+			index
+		}
+	}
 
-impl<S, T> AbstractMaker<T> for S
-where
-    T: AbstractFactory,
-    S: ConceptAdder<T>,
-{
-}
+	impl<S, T> StringMaker<T> for S
+	where
+		T: StringFactory,
+		S: ConceptAdder<T> + StringAdder,
+	{
+	}
+	pub trait AbstractMaker<T>
+	where
+		T: AbstractFactory,
+		Self: ConceptAdder<T>,
+	{
+		fn new_abstract(&mut self) -> usize {
+		    let concept = T::new_abstract();
+		    self.add_concept(concept)
+		}
+	}
 
-pub trait Expander<T>
-where
-    Self: Display<T> + Reduce<T>,
-    T: GetReduction + GetDefinition + GetDefinitionOf + MaybeString,
-{
-    fn expand<
-        U: MaybeConcept
-            + MightExpand<U>
-            + fmt::Display
-            + Clone
-            + Pair<U>
-            + DisplayJoint
-            + SyntaxFactory,
-    >(
-        &self,
-        ast: &U,
-    ) -> U {
-        if let Some(con) = ast.get_concept() {
-            if let Some((left, right)) = self.read_concept(con).get_definition() {
-                self.combine(
-                    &self.expand(&self.to_ast::<U>(left)),
-                    &self.expand(&self.to_ast::<U>(right)),
-                )
-            } else {
-                self.to_ast::<U>(con)
-            }
-        } else if let Some((ref left, ref right)) = ast.get_expansion() {
-            self.combine(&self.expand(left), &self.expand(right))
-        } else {
-            ast.clone()
-        }
-    }
-}
-
-impl<S, T> Expander<T> for S
-where
-    S: Display<T> + Reduce<T>,
-    T: GetReduction + GetDefinition + GetDefinitionOf + MaybeString,
-{
-}
-
-pub trait GetLabel<T>
-where
-    T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
-    Self: GetNormalForm<T> + GetConceptOfLabel<T>,
-{
-    fn get_label(&self, concept: usize) -> Option<String> {
-        match self.get_concept_of_label(concept) {
-            None => None,
-            Some(d) => match self.get_normal_form(d) {
-                None => None,
-                Some(n) => self.read_concept(n).get_string(),
-            },
-        }
-    }
-}
-
-impl<S, T> GetLabel<T> for S
-where
-    T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
-    S: GetNormalForm<T> + GetConceptOfLabel<T>,
-{
-}
-
-pub trait Display<T>
-where
-    Self: GetLabel<T>,
-    T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
-{
-    fn display(&self, concept: usize) -> String {
-        match self.read_concept(concept).get_string() {
-            Some(s) => "\"".to_string() + &s + "\"",
-            None => match self.get_label(concept) {
-                Some(l) => l,
-                None => match self.read_concept(concept).get_definition() {
-                    Some((left, right)) => {
-                        let mut left_string = self.display(left);
-                        if left_string.contains(' ') {
-                            left_string = "(".to_string() + &left_string;
-                        }
-                        let mut right_string = self.display(right);
-                        if right_string.contains(' ') {
-                            right_string += ")";
-                        }
-                        left_string + " " + &right_string
-                    }
-                    None => panic!("Unlabelled concept with no definition!"),
-                },
-            },
-        }
-    }
-}
-
-impl<S, T> Display<T> for S
-where
-    S: GetLabel<T>,
-    T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
-{
-}
-
-pub trait Reduce<T>
-where
-    Self: GetLabel<T> + Combine<T>,
-    T: GetDefinitionOf + GetDefinition + GetReduction + MaybeString,
-{
-    fn recursively_reduce<
-        U: SyntaxFactory + MightExpand<U> + Clone + Pair<U> + MaybeConcept + DisplayJoint,
-    >(
-        &self,
-        ast: &U,
-    ) -> U {
-        match self.reduce(ast) {
-            Some(ref a) => self.recursively_reduce(a),
-            None => ast.clone(),
-        }
-    }
-    fn reduce<U: SyntaxFactory + MightExpand<U> + Clone + Pair<U> + MaybeConcept + DisplayJoint>(
-        &self,
-        ast: &U,
-    ) -> Option<U> {
-        match ast.get_concept() {
-            Some(c) => self.reduce_concept::<U>(c),
-            None => match ast.get_expansion() {
-                None => None,
-                Some((ref left, ref right)) => {
-                    self.match_left_right::<U>(self.reduce(left), self.reduce(right), left, right)
-                }
-            },
-        }
-    }
-    fn reduce_concept<U: SyntaxFactory + Clone + Pair<U> + MaybeConcept + DisplayJoint>(
-        &self,
-        concept: usize,
-    ) -> Option<U> {
-        match self.get_normal_form(concept) {
-            None => match self.read_concept(concept).get_definition() {
-                Some((left, right)) => {
-                    let left_result = self.reduce_concept::<U>(left);
-                    let right_result = self.reduce_concept::<U>(right);
-                    self.match_left_right::<U>(
-                        left_result,
-                        right_result,
-                        &self.to_ast::<U>(left),
-                        &self.to_ast::<U>(right),
-                    )
-                }
-                None => None,
-            },
-            Some(n) => Some(self.to_ast::<U>(n)),
-        }
-    }
-    fn to_ast<U: SyntaxFactory + Clone + Pair<U> + MaybeConcept + DisplayJoint>(
-        &self,
-        concept: usize,
-    ) -> U {
-        match self.get_label(concept) {
-            Some(ref s) => U::new(s, Some(concept)),
-            None => match self.read_concept(concept).get_definition() {
-                Some((left, right)) => {
-                    self.combine(&self.to_ast::<U>(left), &self.to_ast::<U>(right))
-                }
-                None => panic!("Unlabelled concept with no definition"),
-            },
-        }
-    }
-    fn match_left_right<U: Pair<U> + MaybeConcept + DisplayJoint>(
-        &self,
-        left: Option<U>,
-        right: Option<U>,
-        original_left: &U,
-        original_right: &U,
-    ) -> Option<U> {
-        match (left, right) {
-            (None, None) => None,
-            (Some(new_left), None) => Some(self.contract_pair::<U>(&new_left, original_right)),
-            (None, Some(new_right)) => Some(self.contract_pair::<U>(original_left, &new_right)),
-            (Some(new_left), Some(new_right)) => {
-                Some(self.contract_pair::<U>(&new_left, &new_right))
-            }
-        }
-    }
-    fn contract_pair<U: MaybeConcept + Pair<U> + DisplayJoint>(
-        &self,
-        lefthand: &U,
-        righthand: &U,
-    ) -> U {
-        if let (Some(lc), Some(rc)) = (lefthand.get_concept(), righthand.get_concept()) {
-            if let Some(def) = self.find_definition(lc, rc) {
-                if let Some(ref a) = self.get_label(def) {
-                    return U::from_pair(a, Some(def), lefthand, righthand);
-                }
-            }
-        }
-        self.combine(lefthand, righthand)
-    }
-}
-
-impl<S, T> Reduce<T> for S
-where
-    S: GetLabel<T> + Combine<T>,
-    T: GetDefinitionOf + GetDefinition + MaybeString + GetReduction,
-{
+	impl<S, T> AbstractMaker<T> for S
+	where
+		T: AbstractFactory,
+		S: ConceptAdder<T>,
+	{
+	}
 }
 
 mod concept_reader {
-	pub use ast::traits::{DisplayJoint, MaybeConcept, Pair};
+	pub use ast::traits::{DisplayJoint, MaybeConcept, MightExpand, Pair, SyntaxFactory};
 	pub use context::traits::ConceptReader;
-	pub use concepts::traits::{GetReduction, GetDefinition, GetDefinitionOf, FindWhatReducesToIt};
+	pub use concepts::traits::{MaybeString, GetReduction, GetDefinition, GetDefinitionOf, FindWhatReducesToIt};
 	use constants::LABEL;
+	use std::fmt;
+	pub trait Expander<T>
+	where
+		Self: Reduce<T>,
+		T: GetReduction + GetDefinition + GetDefinitionOf + MaybeString,
+	{
+		fn expand<
+		    U: MaybeConcept
+		        + MightExpand<U>
+		        + fmt::Display
+		        + Clone
+		        + Pair<U>
+		        + DisplayJoint
+		        + SyntaxFactory,
+		>(
+		    &self,
+		    ast: &U,
+		) -> U {
+		    if let Some(con) = ast.get_concept() {
+		        if let Some((left, right)) = self.read_concept(con).get_definition() {
+		            self.combine(
+		                &self.expand(&self.to_ast::<U>(left)),
+		                &self.expand(&self.to_ast::<U>(right)),
+		            )
+		        } else {
+		            self.to_ast::<U>(con)
+		        }
+		    } else if let Some((ref left, ref right)) = ast.get_expansion() {
+		        self.combine(&self.expand(left), &self.expand(right))
+		    } else {
+		        ast.clone()
+		    }
+		}
+	}
+
+	impl<S, T> Expander<T> for S
+	where
+		S: Reduce<T>,
+		T: GetReduction + GetDefinition + GetDefinitionOf + MaybeString,
+	{
+	}
+	pub trait Reduce<T>
+	where
+		Self: GetLabel<T> + Combine<T>,
+		T: GetDefinitionOf + GetDefinition + GetReduction + MaybeString,
+	{
+		fn recursively_reduce<
+			U: SyntaxFactory + MightExpand<U> + Clone + Pair<U> + MaybeConcept + DisplayJoint,
+		>(
+			&self,
+			ast: &U,
+		) -> U {
+			match self.reduce(ast) {
+			    Some(ref a) => self.recursively_reduce(a),
+			    None => ast.clone(),
+			}
+		}
+		fn reduce<U: SyntaxFactory + MightExpand<U> + Clone + Pair<U> + MaybeConcept + DisplayJoint>(
+			&self,
+			ast: &U,
+		) -> Option<U> {
+			match ast.get_concept() {
+			    Some(c) => self.reduce_concept::<U>(c),
+			    None => match ast.get_expansion() {
+			        None => None,
+			        Some((ref left, ref right)) => {
+			            self.match_left_right::<U>(self.reduce(left), self.reduce(right), left, right)
+			        }
+			    },
+			}
+		}
+		fn reduce_concept<U: SyntaxFactory + Clone + Pair<U> + MaybeConcept + DisplayJoint>(
+			&self,
+			concept: usize,
+		) -> Option<U> {
+			match self.get_normal_form(concept) {
+			    None => match self.read_concept(concept).get_definition() {
+			        Some((left, right)) => {
+			            let left_result = self.reduce_concept::<U>(left);
+			            let right_result = self.reduce_concept::<U>(right);
+			            self.match_left_right::<U>(
+			                left_result,
+			                right_result,
+			                &self.to_ast::<U>(left),
+			                &self.to_ast::<U>(right),
+			            )
+			        }
+			        None => None,
+			    },
+			    Some(n) => Some(self.to_ast::<U>(n)),
+			}
+		}
+		fn to_ast<U: SyntaxFactory + Clone + Pair<U> + MaybeConcept + DisplayJoint>(
+			&self,
+			concept: usize,
+		) -> U {
+			match self.get_label(concept) {
+			    Some(ref s) => U::new(s, Some(concept)),
+			    None => match self.read_concept(concept).get_definition() {
+			        Some((left, right)) => {
+			            self.combine(&self.to_ast::<U>(left), &self.to_ast::<U>(right))
+			        }
+			        None => panic!("Unlabelled concept with no definition"),
+			    },
+			}
+		}
+		fn match_left_right<U: Pair<U> + MaybeConcept + DisplayJoint>(
+			&self,
+			left: Option<U>,
+			right: Option<U>,
+			original_left: &U,
+			original_right: &U,
+		) -> Option<U> {
+			match (left, right) {
+			    (None, None) => None,
+			    (Some(new_left), None) => Some(self.contract_pair::<U>(&new_left, original_right)),
+			    (None, Some(new_right)) => Some(self.contract_pair::<U>(original_left, &new_right)),
+			    (Some(new_left), Some(new_right)) => {
+			        Some(self.contract_pair::<U>(&new_left, &new_right))
+			    }
+			}
+		}
+		fn contract_pair<U: MaybeConcept + Pair<U> + DisplayJoint>(
+			&self,
+			lefthand: &U,
+			righthand: &U,
+		) -> U {
+			if let (Some(lc), Some(rc)) = (lefthand.get_concept(), righthand.get_concept()) {
+			    if let Some(def) = self.find_definition(lc, rc) {
+			        if let Some(ref a) = self.get_label(def) {
+			            return U::from_pair(a, Some(def), lefthand, righthand);
+			        }
+			    }
+			}
+			self.combine(lefthand, righthand)
+		}
+	}
+
+	impl<S, T> Reduce<T> for S
+	where
+		S: GetLabel<T> + Combine<T>,
+		T: GetDefinitionOf + GetDefinition + MaybeString + GetReduction,
+	{
+	}
+	pub trait Display<T>
+	where
+		Self: GetLabel<T>,
+		T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
+	{
+		fn display(&self, concept: usize) -> String {
+		    match self.read_concept(concept).get_string() {
+		        Some(s) => "\"".to_string() + &s + "\"",
+		        None => match self.get_label(concept) {
+		            Some(l) => l,
+		            None => match self.read_concept(concept).get_definition() {
+		                Some((left, right)) => {
+		                    let mut left_string = self.display(left);
+		                    if left_string.contains(' ') {
+		                        left_string = "(".to_string() + &left_string;
+		                    }
+		                    let mut right_string = self.display(right);
+		                    if right_string.contains(' ') {
+		                        right_string += ")";
+		                    }
+		                    left_string + " " + &right_string
+		                }
+		                None => panic!("Unlabelled concept with no definition!"),
+		            },
+		        },
+		    }
+		}
+	}
+
+	impl<S, T> Display<T> for S
+	where
+		S: GetLabel<T>,
+		T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
+	{
+	}
+	pub trait GetLabel<T>
+	where
+		T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
+		Self: GetNormalForm<T> + GetConceptOfLabel<T>,
+	{
+		fn get_label(&self, concept: usize) -> Option<String> {
+		    match self.get_concept_of_label(concept) {
+		        None => None,
+		        Some(d) => match self.get_normal_form(d) {
+		            None => None,
+		            Some(n) => self.read_concept(n).get_string(),
+		        },
+		    }
+		}
+	}
+
+	impl<S, T> GetLabel<T> for S
+	where
+		T: MaybeString + GetDefinitionOf + GetDefinition + GetReduction,
+		S: GetNormalForm<T> + GetConceptOfLabel<T>,
+	{
+	}
 	pub trait Combine<T>
 	where
 		Self: FindDefinition<T>,
