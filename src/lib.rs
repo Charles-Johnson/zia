929aaa548dd5bd23a023c92df8a9d5026b66c9ad
Charles Johnson <charlesthomasjohnson0@gmail.com>
2018-12-20 20:59:44 +0900

    Make 'removing' module its own file.

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ mod concepts;
 mod constants;
 mod context;
 mod reading;
+mod removing;
 mod token;
 mod utils;
 mod writing;
@@ -512,70 +513,4 @@ where
         + MaybeString,
     S: ConceptMaker<T> + GetLabel<T> + DefinitionDeleter<T>,
 {
-}
-
-mod removing {
-	use context::{BlindConceptRemover, StringRemover};
-	use reading::{FindWhatReducesToIt, MaybeString, MaybeDisconnected};
-	use writing::{Unlabeller, DeleteDefinition, ConceptReader, RemoveReduction, GetDefinitionOf, GetDefinition, RemoveDefinition, GetReduction};
-	pub trait DefinitionDeleter<T>
-	where
-		Self: MaybeDisconnected<T> + ConceptRemover<T> + DeleteDefinition<T> + Unlabeller<T>,
-		T: RemoveDefinition
-		    + RemoveReduction
-		    + GetDefinitionOf
-		    + GetDefinition
-		    + FindWhatReducesToIt
-		    + GetReduction
-		    + MaybeString,
-	{
-		fn cleanly_delete_definition(&mut self, concept: usize) {
-		    let definition = self.read_concept(concept).get_definition();
-		    self.delete_definition(concept);
-		    self.try_delete_concept(concept);
-		    if let Some((left, right)) = definition {
-		        self.try_delete_concept(left);
-		        self.try_delete_concept(right);
-		    }
-		}
-		fn try_delete_concept(&mut self, concept: usize) {
-		    if self.is_disconnected(concept) {
-		        self.unlabel(concept);
-		        self.remove_concept(concept);
-		    }
-		}
-	}
-
-	impl<S, T> DefinitionDeleter<T> for S
-	where
-		S: MaybeDisconnected<T> + ConceptRemover<T> + DeleteDefinition<T> + Unlabeller<T>,
-		T: RemoveDefinition
-		    + RemoveReduction
-		    + GetDefinitionOf
-		    + GetDefinition
-		    + FindWhatReducesToIt
-		    + GetReduction
-		    + MaybeString,
-	{
-	}
-
-	pub trait ConceptRemover<T>
-	where
-		Self: BlindConceptRemover + ConceptReader<T> + StringRemover,
-		T: MaybeString,
-	{
-		fn remove_concept(&mut self, concept: usize) {
-		    if let Some(ref s) = self.read_concept(concept).get_string() {
-		        self.remove_string(s);
-		    }
-		    self.blindly_remove_concept(concept);
-		}
-	}
-
-	impl<S, T> ConceptRemover<T> for S
-	where
-		S: BlindConceptRemover + ConceptReader<T> + StringRemover,
-		T: MaybeString,
-	{
-	}
 }
